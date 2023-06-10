14ce710a3ab8f4fb40c7d3ca9d9f4477ba8a0c3f
Meobot <nathanrhodes312@gmail.com>
2023-06-09 23:20:04 -0400

    able to select answer now, still needs collection

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Quiz(props) {
+
+	const correctAnswers = props.quizData.map((question) => question.correct_answer);
+	console.log("🚀 ~ file: Quiz.tsx:6 ~ Quiz ~ correctAnswers:", correctAnswers)
+
 	return (
-		<div className="flex flex-col container">
+		<div className="flex flex-col container p-8">
 			{props.quizData.map((question) => (
 				<div
 					key={question.question}
@@ -11,26 +15,38 @@ function Quiz(props) {
 					<h2 className="font-karla leading-5 text-xl">
 						{question.question}
 					</h2>
-					<div className="space-x-6">
-						{/* map over correct and incorrect answers and shuffle them, then display all answers */}
+					<div className="flex flex-wrap space-x-6">
 						{[
 							...question.incorrect_answers,
 							question.correct_answer,
 						]
 							.sort(() => Math.random() - 0.5)
-							.map((answer) => (
-								<button
-									className="border-solid border-2 border-btnBorColor rounded-2xl min-w-1/2 py-2 px-5 my-5"
-									key={answer}
+							.map((choice) => (
+								<div 
+								key={choice}
+								className="my-6"
 								>
-									{answer}
-								</button>
+									<input
+										type={props.type === "multiple" ? "checkbox" : "radio"}
+										id={choice}
+										name={question.question}
+										value={choice}
+									/>
+									<label
+										htmlFor={choice}
+										className="border-solid border-2 border-btnBorColor rounded-2xl min-w-1/2 py-2 px-5"
+									>
+										{choice}
+									</label>
+								</div>
 							))}
 					</div>
 				</div>
 			))}
 			<div className="m-auto">
-				<button className="bg-btnBorColor text-white rounded-2xl px-7 py-4">Check answers</button>
+				<button className="bg-btnBorColor text-white rounded-2xl px-7 py-4">
+					Check answers
+				</button>
 			</div>
 		</div>
 	);
