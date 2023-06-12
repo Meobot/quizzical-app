5b1dce889fd1c4d06c50a70070193f9f36ab3848
Meobot <nathanrhodes312@gmail.com>
2023-06-11 21:26:19 -0400

    score renders / play again added

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,48 +1,116 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
 
 function Quiz(props) {
-  return (
-    <form className="flex flex-col container p-8">
-      {props.quizData.map((question) => (
-        <div key={question.question} className="border-b-2 border-quizBorderColor mb-5">
-          <h2 className="font-karla leading-5 text-xl">{question.question}</h2>
-          <div className="flex flex-wrap space-x-6">
-            {[
-              ...question.incorrect_answers,
-              question.correct_answer,
-            ]
-              .sort(() => Math.random() - 0.5)
-              .map((answer) => {
-                const inputId = nanoid(); 
+	const [selectedAnswers, setSelectedAnswers] = useState({});
+	const [shuffledAnswers, setShuffledAnswers] = useState([]);
+	const [score, setScore] = useState(0);
+	const [gameEnd, setGameEnd] = useState(false);
 
-                return (
-                  <div key={answer} className="my-6">
-                    <input
-                      type="radio"
-                      id={inputId}
-                      name={question.question}
-                      value={answer}
-                    />
-                    <label
-                      htmlFor={inputId}
-                      className="border-solid border-2 border-btnBorColor rounded-2xl min-w-1/2 py-2 px-5"
-                    >
-                      {answer}
-                    </label>
-                  </div>
-                );
-              })}
-          </div>
-        </div>
-      ))}
-      <div className="m-auto">
-        <button className="bg-btnBorColor text-white rounded-2xl px-7 py-4">
-          Check answers
-        </button>
-      </div>
-    </form>
-  );
+	useEffect(() => {
+		shuffleAnswers();
+	}, []);
+
+	const shuffleAnswers = () => {
+		const shuffled = props.quizData.map((question) => {
+			const answers = [
+				...question.incorrect_answers,
+				question.correct_answer,
+			];
+			return {
+				...question,
+				answers: answers.sort(() => Math.random() - 0.5),
+			};
+		});
+
+		setShuffledAnswers(shuffled);
+	};
+
+	const handleAnswerSelect = (question, answer) => {
+		setSelectedAnswers((prevAnswers) => ({
+			...prevAnswers,
+			[question]: answer,
+		}));
+	};
+
+	const checkAnswers = (e) => {
+		e.preventDefault();
+		const { quizData } = props;
+		const correctAnswers = quizData.map(
+			(question) => question.correct_answer
+		);
+		let userScore = 0;
+
+		for (let i = 0; i < quizData.length; i++) {
+			if (selectedAnswers[quizData[i].question] === correctAnswers[i]) {
+				userScore++;
+			}
+		}
+
+		setScore(userScore);
+		setGameEnd(true);
+	};
+
+	return (
+		<form className="flex flex-col container p-8">
+			{shuffledAnswers.map((question) => (
+				<div
+					key={question.question}
+					className="border-b-2 border-quizBorderColor mb-5"
+				>
+					<h2 className="font-karla leading-5 text-xl">
+						{question.question}
+					</h2>
+					<div className="flex flex-wrap space-x-6">
+						{question.answers.map((answer) => {
+							const inputId = nanoid();
+
+							return (
+								<div key={answer} className="my-6">
+									<input
+										type="radio"
+										id={inputId}
+										name={question.question}
+										value={answer}
+										onChange={() =>
+											handleAnswerSelect(
+												question.question,
+												answer
+											)
+										}
+										checked={
+											selectedAnswers[
+												question.question
+											] === answer
+										}
+									/>
+									<label
+										htmlFor={inputId}
+										className="border-solid border-2 border-btnBorColor rounded-2xl min-w-1/2 py-2 px-5"
+									>
+										{answer}
+									</label>
+								</div>
+							);
+						})}
+					</div>
+				</div>
+			))}
+			<div className="flex justify-center items-center  space-x-14">
+				{gameEnd && (
+						<h3 className="font-bold">
+							You scored {score}/5 correct answers
+						</h3>
+				)}
+				<button
+					className="bg-btnBorColor text-white rounded-2xl px-7 py-4"
+					onClick={gameEnd ? props.playAgain : checkAnswers}
+				>
+					{gameEnd ? "Play again" : "Check answers"}
+				</button>
+			</div>
+		</form>
+	);
 }
 
 export default Quiz;
