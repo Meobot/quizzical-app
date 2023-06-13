bc7327034b00bd430857f47f416da1c19b892004
Meobot <nathanrhodes312@gmail.com>
2023-06-12 22:02:04 -0400

    types added

--- src/components/Start.tsx
+++ src/components/Start.tsx
@@ -1,44 +1,61 @@
-import { categoryOptions } from "../data/categoryOptions.ts";
+import { categoryOptions } from "../data/categoryOptions";
 
-function Start(props) {
+interface StartProps {
+  setGameStart: React.Dispatch<React.SetStateAction<boolean>>;
+  handleFormChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+}
 
-	const handleBtnClick = (e: { preventDefault: () => void; }) => {
-		e.preventDefault();
-		props.setGameStart(true);
-	};
+function Start(props: StartProps) {
+  const handleBtnClick = (e: React.FormEvent) => {
+    e.preventDefault();
+    props.setGameStart(true);
+  };
 
-	return (
-		<div className="flex flex-col space-y-5 bg-sky-300 p-7 rounded-2xl items-center">
-			<h1 className="font-bold text-3xl">Quizzical</h1>
-			<form className="flex flex-col space-y-7">
-				<div className="flex flex-col space-y-2">
-					<label htmlFor="category">Category</label>
-					<select name="category" id="category" onChange={props.handleFormChange}>
-						<option value="any">Any</option>
-						{categoryOptions.map((category) => (
-							<option key={category.id} value={category.id}>
-								{category.name}
-							</option>
-						))}
-					</select>
-					<label htmlFor="difficulty">Difficulty</label>
-					<select name="difficulty" id="difficulty" onChange={props.handleFormChange}>
-						<option value="any">Any</option>
-						<option value="easy">Easy</option>
-						<option value="medium">Medium</option>
-						<option value="hard">Hard</option>
-					</select>
-					<label htmlFor="type">Type</label>
-					<select name="type" id="type" onChange={props.handleFormChange}>
-						<option value="any">Any</option>
-						<option value="multiple">Multiple Choice</option>
-						<option value="boolean">True / False</option>
-					</select>
-				</div>
-				<button className="bg-btnBorColor text-white rounded-2xl py-5" onClick={handleBtnClick}>Start Quiz</button>
-			</form>
-		</div>
-	);
+  return (
+    <div className="flex flex-col space-y-5 bg-sky-300 p-7 rounded-2xl items-center">
+      <h1 className="font-bold text-3xl">Quizzical</h1>
+      <form className="flex flex-col space-y-7">
+        <div className="flex flex-col space-y-2">
+          <label htmlFor="category">Category</label>
+          <select
+            name="category"
+            id="category"
+            onChange={props.handleFormChange}
+          >
+            <option value="any">Any</option>
+            {categoryOptions.map((category) => (
+              <option key={category.id} value={category.id}>
+                {category.name}
+              </option>
+            ))}
+          </select>
+          <label htmlFor="difficulty">Difficulty</label>
+          <select
+            name="difficulty"
+            id="difficulty"
+            onChange={props.handleFormChange}
+          >
+            <option value="any">Any</option>
+            <option value="easy">Easy</option>
+            <option value="medium">Medium</option>
+            <option value="hard">Hard</option>
+          </select>
+          <label htmlFor="type">Type</label>
+          <select name="type" id="type" onChange={props.handleFormChange}>
+            <option value="any">Any</option>
+            <option value="multiple">Multiple Choice</option>
+            <option value="boolean">True / False</option>
+          </select>
+        </div>
+        <button
+          className="bg-btnBorColor text-white rounded-2xl py-5"
+          onClick={handleBtnClick}
+        >
+          Start Quiz
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Start;
